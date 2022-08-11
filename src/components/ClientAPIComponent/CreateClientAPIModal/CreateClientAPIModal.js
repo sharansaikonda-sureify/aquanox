7b051df860617fac389aa20f4728713d5d4c999a
Sharan Konda <sharansai.konda@sureify.com>
2022-08-11 18:47:25 +0530

    add pasting functionality to popup

--- src/components/ClientAPIComponent/CreateClientAPIModal/CreateClientAPIModal.js
+++ src/components/ClientAPIComponent/CreateClientAPIModal/CreateClientAPIModal.js
@@ -1,6 +1,7 @@
 // Packages
 import React, { useEffect, useState } from "react";
 import { Modal } from "react-bootstrap";
+import { v4 as uuidv4 } from "uuid";
 
 // Custom Objects
 import { ClientAPI } from "../../../constants/clientapi";
@@ -14,6 +15,9 @@ import { Stack, Button } from "@mui/material";
 // Material UI Icons
 import { Check, Close } from "@mui/icons-material";
 
+// Material UI Colors
+import { deepOrange } from "@mui/material/colors";
+
 const CreateClientAPIModal = ({
   createNewClientAPI,
   createMappingState,
@@ -24,6 +28,18 @@ const CreateClientAPIModal = ({
 }) => {
   const [data, setData] = useState(new ClientAPI(createMappingState.mapping));
 
+  const pasteMappings = async (e) => {
+    try {
+      const copiedDataStr = await navigator.clipboard.readText();
+      const copiedData = JSON.parse(copiedDataStr);
+      const mapping = new ClientAPI(copiedData);
+      mapping.client_data_source_uuid = uuidv4();
+      setData(mapping);
+    } catch (e) {
+      setErrorState({ error: e, showModal: true });
+    }
+  };
+
   // Reset state when modal is closed
   useEffect(() => {
     setData(new ClientAPI(createMappingState.mapping));
@@ -32,7 +48,20 @@ const CreateClientAPIModal = ({
   return (
     <Modal size="xl" show={createMappingState.showModal} onHide={closeModal}>
       <Modal.Header closeButton>
-        <Modal.Title>JSON Payload</Modal.Title>
+        <Modal.Title>
+          JSON Payload
+          <Button
+            sx={{
+              color: deepOrange[900],
+              "&:hover": {
+                color: deepOrange[800],
+              },
+            }}
+            onClick={pasteMappings}
+          >
+            Paste
+          </Button>
+        </Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <ClientAPIObjectMapping
--- src/components/SureifyObjectMappingComponent/CreateMappingModal/CreateMappingModal.js
+++ src/components/SureifyObjectMappingComponent/CreateMappingModal/CreateMappingModal.js
@@ -1,6 +1,7 @@
 // Packages
 import React, { useEffect, useState } from "react";
 import { Modal } from "react-bootstrap";
+import { v4 as uuidv4 } from "uuid";
 
 // Custom Objects
 import { SureifyObjectMapping } from "../../../constants/som";
@@ -14,11 +15,26 @@ import { Check, Close } from "@mui/icons-material";
 // Material UI Components
 import { Button, Stack } from "@mui/material";
 
+// Material UI Colors
+import { deepOrange } from "@mui/material/colors";
+
 const CreateMappingModal = React.memo(
   ({ createNewMappings, createMappingState, closeModal, setErrorState }) => {
     const [data, setData] = useState(
       new SureifyObjectMapping(createMappingState.mapping)
     );
+
+    const pasteMappings = async (e) => {
+      try {
+        const copiedDataStr = await navigator.clipboard.readText();
+        const copiedData = JSON.parse(copiedDataStr);
+        const mapping = new SureifyObjectMapping(copiedData.mappings[0]);
+        mapping.mapping_id = uuidv4();
+        setData(mapping);
+      } catch (e) {
+        setErrorState({ error: e, showModal: true });
+      }
+    };
 
     // Reset state when modal is closed
     useEffect(() => {
@@ -28,7 +44,20 @@ const CreateMappingModal = React.memo(
     return (
       <Modal size="xl" show={createMappingState.showModal} onHide={closeModal}>
         <Modal.Header closeButton>
-          <Modal.Title>JSON Payload</Modal.Title>
+          <Modal.Title>
+            JSON Payload
+            <Button
+              sx={{
+                color: deepOrange[900],
+                "&:hover": {
+                  color: deepOrange[800],
+                },
+              }}
+              onClick={pasteMappings}
+            >
+              Paste
+            </Button>
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <ObjectMapping
